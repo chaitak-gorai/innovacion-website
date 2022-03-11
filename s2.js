d01b2c11d5bb740d4c26ee241dbadd9793fb21c7
chaitak-gorai <chaitakgorai@gmail.com>
2022-03-11 16:25:25 +0530

    🔥 card ui changed

--- s2.js
+++ s2.js
@@ -1,11 +1,11 @@
 function sponsorsdata() {
   const itemsDiv1 = document.querySelector('#spon');
   let content1 = sponsors.map((data) => {
-    return `<div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="supporter-logo">
+    return `<div class="col-lg-3 col-md-4 col-xs-6" style="margin-top:20px">
+            <div class="supporter-logo" style="background-color:white">
               <img src="./assets/previous_year_sponsors/${data.imgsrc}" class="img-fluid" alt=""/>
             </div>
-          </div>`
+          </div>`;
   });
   content1 = content1.join(' ');
   itemsDiv1.innerHTML = content1;
